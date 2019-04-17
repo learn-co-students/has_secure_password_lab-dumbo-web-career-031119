123f46590ead05346aa4d5d007805a8ba969a75a
jwy5140 <jwy5140@gmail.com>
2019-04-17 00:33:57 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get '/homepage', to: "users#homepage" 
+
+  resources :sessions, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
